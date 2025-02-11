ae4455c7aa5f982f9491dea293ecd386fae73939
Felipesv1 <fepereirasilva4@gmail.com>
2025-02-11 16:28:05 -0300

    finalizado style component reembolso

--- src/components/reembolso/Reembolsos.jsx
+++ src/components/reembolso/Reembolsos.jsx
@@ -2,7 +2,13 @@ import Navbar from "../navbar/Navbar.jsx";
 import style from "./Reembolsos.module.scss";
 import Home_icon from "../../assets/Dashboard/icon_Home.png";
 import image_refund_request from "../../assets/Dashboard/Solicitar_Reembolso.png";
-import n_request from "../../assets/Dashboard/N-Solicitados.png";
+import image_history_request from "../../assets/Dashboard/Solicitar_Histórico.png";
+import image_analyzing_verify from "../../assets/Dashboard/Análises.png";
+import image_analyzing from "../../assets/Dashboard/N-Análises.png";
+import image_request from "../../assets/Dashboard/N-Solicitados.png";
+import image_aproved from "../../assets/Dashboard/N-Aprovados.png";
+import image_rejected from "../../assets/Dashboard/N-Rejeitados.png";
+import image_update from "../../assets/Dashboard/atualizado.png";
 export default function Reembolsos() {
   return (
     <section>
@@ -13,11 +19,13 @@ export default function Reembolsos() {
       </div>
       <main>
         <section className={style.container__dashboard}>
-          <h1>Sistema de Reembolsos</h1>
-          <p>
-            Solicite novos pedidos de reembolso, visualize solicitações em
-            análise e todo o histórico.
-          </p>
+          <div>
+            <h1>Sistema de Reembolsos</h1>
+            <p>
+              Solicite novos pedidos de reembolso, visualize solicitações em
+              análise e todo o histórico.
+            </p>
+          </div>
           <section className={style.container_cards}>
             <div className={style.card}>
               <img
@@ -28,25 +36,25 @@ export default function Reembolsos() {
             </div>
             <div className={style.card}>
               <img
-                src={image_refund_request}
-                alt="imagem de solicitar reembolso"
+                src={image_analyzing_verify}
+                alt="imagem de verificar analise"
               />
-              <p>Solicitar Reembolso</p>
+              <p>Verificar análises</p>
             </div>
             <div className={style.card}>
               <img
-                src={image_refund_request}
-                alt="imagem de solicitar reembolso"
+                src={image_history_request}
+                alt="imagem de solicitar histórico"
               />
-              <p>Solicitar Reembolso</p>
+              <p>Hístorico</p>
             </div>
           </section>
           <section className={style.data_dashboard}>
             <div className={style.data}>
-              <div>
+              <div className={style.data__icon_n_request}>
                 <img
-                  src={n_request}
-                  alt="seta indicando o numero de solicitações"
+                  src={image_request}
+                  alt="icone de solicitação de reembolso"
                 />
               </div>
               <span>
@@ -55,43 +63,46 @@ export default function Reembolsos() {
               </span>
             </div>
             <div className={style.data}>
-              <div>
+              <div className={style.data__icon_n_analyzing}>
                 <img
-                  src={n_request}
-                  alt="seta indicando o numero de solicitações"
+                  src={image_analyzing}
+                  alt="icone de analise de solicitações"
                 />
               </div>
               <span>
-                <b>182</b>
-                {""} solicitados
+                <b>74</b>
+                {""} Em análise
               </span>
             </div>
             <div className={style.data}>
-              <div>
+              <div className={style.data__icon_n_aproved}>
                 <img
-                  src={n_request}
-                  alt="seta indicando o numero de solicitações"
+                  src={image_aproved}
+                  alt="icone de aprovação de solicitações"
                 />
               </div>
               <span>
-                <b>182</b>
-                {""} solicitados
+                <b>195</b>
+                {""} Aprovados
               </span>
             </div>
             <div className={style.data}>
-              <div>
+              <div className={style.data__icon_n_rejected}>
                 <img
-                  src={n_request}
-                  alt="seta indicando o numero de solicitações"
+                  src={image_rejected}
+                  alt="icone de rejeição de solicitações"
                 />
               </div>
               <span>
-                <b>182</b>
-                {""} solicitados
+                <b>41</b>
+                {""} Rejeitados
               </span>
             </div>
           </section>
-          <p></p>
+          <div className={style.container_update}>
+            <img src={image_update} alt="icone chec" />
+            <p className={style.check}>Sistema atualizado.</p>
+          </div>
         </section>
       </main>
     </section>
