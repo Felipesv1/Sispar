6c15197a2efe4d9e08bca8460d2cb82a90277ea4
Felipesv1 <fepereirasilva4@gmail.com>
2025-02-12 17:05:36 -0300

    router implemeted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-// import Login from "./components/login/Login.jsx";
+import Login from "./components/login/Login.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Reembolsos from "./components/reembolso/Reembolsos.jsx";
+import Solicitar_reembolso from "./components/solicitacao/Solicitacao.jsx";
 import "../src/global.scss";
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      <Reembolsos />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/reembolso" element={<Reembolsos />} />
+          <Route
+            path="/solicitar-reembolso"
+            element={<Solicitar_reembolso />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
